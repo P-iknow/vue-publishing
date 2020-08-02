1d6fe6af636bf5e20d9359a63443e2c886a68ea2
P-iknow <apricotsoul@gmail.com>
2020-08-02 17:28:21 +0900

    [Layout03][Feat]

---
+++ web-standard/src/pages/Layout03/Layout03.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrap">
+    <div class="main">
+        <div id="side">side</div>
+        <div id="header">contents</div>
+        <div id="contents">contents</div>
+        <div id="footer">footer</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+
+</script>
+<style lang="scss" src="./Layout03.scss" scoped></style>
--- web-standard/src/router/routes.js
+++ web-standard/src/router/routes.js
@@ -3,6 +3,7 @@ import homePage from '../pages/Home.vue'
 import notFoundPage from '../pages/NotFound.vue'
 import layout01 from '../pages/Layout01'
 import layout02 from '../pages/Layout02'
+import layout03 from '../pages/Layout03'
 
 import { routePropResolver } from './util'
 import { DOMAIN_TITLE } from '../.env'
@@ -25,6 +26,12 @@ export const routes = [
     name: 'layout02',
     component: layout02,
     meta: { title: `${DOMAIN_TITLE} | layout02` }
+  },
+  {
+    path: '/layout03',
+    name: 'layout03',
+    component: layout03,
+    meta: { title: `${DOMAIN_TITLE} | layout03` }
   },
   {
     path: '*',
