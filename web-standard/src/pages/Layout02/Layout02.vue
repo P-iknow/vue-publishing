2d9e372c3628f4140ceceb4470612246aaaeb89d
P-iknow <apricotsoul@gmail.com>
2020-08-02 17:27:48 +0900

    [Layout02]mod: clear both adjust

--- web-standard/src/pages/Layout02/Layout02.vue
+++ web-standard/src/pages/Layout02/Layout02.vue
@@ -3,9 +3,9 @@
     <div class="main">
         <div id="header">header</div>
         <div id="nav">nav</div>
-        <div id="side_left">side_left</div>
+        <div id="side_left">side left</div>
         <div id="contents">contents</div>
-        <div id="side_right">side_right</div>
+        <div id="side_right">side right</div>
         <div id="footer">footer</div>
     </div>
   </div>
